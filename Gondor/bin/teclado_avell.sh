8661d4a40e119dbf44ad5456601cafaecc9f7af1
José Antônio Freitas <jfreitas@unb.br>
2020-06-26 18:14:20 -0300

    Atualizando o driver da Wacom.

--- Gondor/bin/teclado_avell.sh
+++ Gondor/bin/teclado_avell.sh
@@ -6,4 +6,9 @@ sudo make install
 sudo install -m644 clevo-xsm-wmi.ko /lib/modules/$(uname -r)/extra
 sudo depmod
 sudo tee /etc/modprobe.d/clevo-xsm-wmi.conf <<< 'options clevo-xsm-wmi kb_color=green,green,green kb_brightness=1 kb_off=0'
+
+cd ~/input-wacom
+
+if test -x ./autogen.sh; then ./autogen.sh; else ./configure; fi && make && sudo make install || echo "Build Failed"
+
 sudo reboot
