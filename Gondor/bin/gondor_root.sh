957847369bc8478265a0c13ac866af99b7fe5dc6
José Antônio Freitas <jfreitas@unb.br>
2020-08-26 18:14:33 -0300

    Não lembro.

--- Gondor/bin/gondor_root.sh
+++ Gondor/bin/gondor_root.sh
@@ -8,7 +8,7 @@ LOG="/Arquivos/OneDrive\ -\ unb.br/Backups/Gondor/"$(date +%Y-%m-%d)"_log-Backup
 
 NOME_BACKUP="gondor_backup_diario_root_"$(date +%Y-%m-%d)".tar.bz2"
 
-LOCAL_TEMPORARIO_ROOT="/ArquivosLinux/Backup_Temporario/Gondor/ROOT/"
+LOCAL_TEMPORARIO_ROOT="/Arquivos/Backup_Temporario/Gondor/ROOT/"
 
 LOCAL_TAR="/ArquivosLinux/Backup_Temporario/"
 
@@ -25,19 +25,19 @@ tar -cvjf $LOCAL_TAR$NOME_BACKUP $LOCAL_TEMPORARIO_ROOT
 
 cp $NOME_BACKUP $LOCAL_BACKUP
 
-chown -R jfreitas:users $LOCAL_BACKUP
+# chown -R jfreitas:users $LOCAL_BACKUP
 
-rm $NOME_BACKUP
+# rm $NOME_BACKUP
 
-HOJE=$(date +'%Y-%m-%d')
+# HOJE=$(date +'%Y-%m-%d')
 
-for filename in $LOCAL_BACKUP*.tar.bz2; do
+# for filename in $LOCAL_BACKUP*.tar.bz2; do
 
-    DATAARQUIVO=$(echo $filename | grep -Eo '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}')
+#     DATAARQUIVO=$(echo $filename | grep -Eo '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}')
 
-    DIFERENCA=$(($(($(date -d $HOJE "+%s") - $(date -d $DATAARQUIVO "+%s")))  / 86400 ))
+#     DIFERENCA=$(($(($(date -d $HOJE "+%s") - $(date -d $DATAARQUIVO "+%s")))  / 86400 ))
 
-    if [[ $DIFERENCA -ge $ROTATIVIDADE ]]; then
-        rm -f $filename
-    fi
-done
+#     if [[ $DIFERENCA -ge $ROTATIVIDADE ]]; then
+#         rm -f $filename
+#     fi
+# done
