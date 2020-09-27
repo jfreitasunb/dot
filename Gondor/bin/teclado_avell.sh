da17ae80bc18a7da49edd0e27517ada34449cac6
José Antônio Freitas <jfreitas@unb.br>
2020-09-27 15:28:10 -0300

    Limpando os arquivos ao final da geração do módulo.

--- Gondor/bin/teclado_avell.sh
+++ Gondor/bin/teclado_avell.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
+##############################################################################################################################
+######## Driver do teclado Avell
+##############################################################################################################################
+
 cd ~/avell_teclado/module
+
 sudo make clean
+
 make
+
 sudo make install
+
 sudo install -m644 clevo-xsm-wmi.ko /lib/modules/$(uname -r)/extra
+
 sudo depmod
+
 sudo tee /etc/modprobe.d/clevo-xsm-wmi.conf <<< 'options clevo-xsm-wmi kb_color=green,green,green kb_brightness=1 kb_off=0'
 
-cd ~/input-wacom
+##############################################################################################################################
+######## Driver Wacom
+##############################################################################################################################
+
+cd ~/scripts
+
+7z -aoa e input-wacom.7z -oinput-wacom/
+
+cd input-wacom
 
 if test -x ./autogen.sh; then ./autogen.sh; else ./configure; fi && make && sudo make install || echo "Build Failed"
+
+cd ../
+
+sudo rm -rf input-wacom
 
 sudo reboot
