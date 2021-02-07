271a5628c7cc205b00a9eb7b02b136f55ffe3e56
José Antônio Freitas <jfreitas@unb.br>
2021-02-07 16:56:27 -0300

    Configurações do fish.

---
+++ Gondor/config/fish/config.fish
@@ -0,0 +1,24 @@
+### EXPORT ###
+set -U fish_user_paths $HOME/.bin $fish_user_paths
+set fish_greeting                      # Supresses fish's intro message
+set TERM "xterm-256color"              # Sets the terminal type
+set EDITOR "vim"      # $EDITOR use Emacs in terminal
+#set VISUAL "emacsclient -c -a emacs"   # $VISUAL use Emacs in GUI mode
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
+
+# Changing "ls" to "exa"
+alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+
+# Changing "cat" to "bat"
+alias cat='bat'
+
+neofetch
