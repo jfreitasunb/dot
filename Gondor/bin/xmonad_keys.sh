f2247192aed60b575574c6e25638a6a01e1c47b3
José Antônio <jfreitas@unb.br>
2021-08-16 19:22:56 -0300

    Removendo marcador das seções.

--- Gondor/bin/xmonad_keys.sh
+++ Gondor/bin/xmonad_keys.sh
@@ -6,4 +6,5 @@ sed -n '/START_KEYS/,/END_KEYS/p' ~/.xmonad/xmonad.hs | \
     grep -v '\-\- , ("' | \
     sed -e 's/^[ \t]*//' \
         -e 's/, (/(/' \
-        -e 's/\[ (/(/'
+        -e 's/\[ (/(/' \
+        -e 's/-- KB_GROUPS /\n/'
