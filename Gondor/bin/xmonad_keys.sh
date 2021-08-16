f893ff82eeebf40b4dbda76fc8332cc72ba3778a
José Antônio <jfreitas@unb.br>
2021-08-16 19:09:55 -0300

    Organizando.

--- Gondor/bin/xmonad_keys.sh
+++ Gondor/bin/xmonad_keys.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash -x
-sed -n '/START_KEYS/,/END_KEYS/p' ~/.xmonad/xmonad.hs | grep -e ', ("' -e '\[ (' | grep -v '\-\- , ("'
+sed -n '/START_KEYS/,/END_KEYS/p' ~/.xmonad/xmonad.hs | \
+    grep -e ', ("' \
+    -e '\[ (' | \
+    grep -v '\-\- , ("'
