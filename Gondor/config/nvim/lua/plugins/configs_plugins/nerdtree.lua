a39b7a117c93e2fbca128fd3b8799a9aa74cfc9c
José Antônio <jfreitas@unb.br>
2021-12-01 15:44:33 -0300

    Configurando o Neovim com o Packer.

---
+++ Gondor/config/nvim/lua/plugins/configs_plugins/nerdtree.lua
@@ -0,0 +1,9 @@
+vim.cmd([[ nnoremap <leader>n :NERDTreeFocus<CR> ]])
+vim.cmd([[ nnoremap <C-n> :NERDTree<CR> ]])
+vim.cmd([[ nnoremap <C-t> :NERDTreeToggle<CR> ]])
+vim.cmd([[ nnoremap <C-f> :NERDTreeFind<CR> ]])
+--vim.cmd([[ nnoremap <space>n :set number!<CR> ]])
+--vim.cmd([[ nnoremap <leader>u :UndotreeShow<CR> ]])
+
+vim.cmd([[ let g:NERDTreeDirArrowExpandable = '󰍝' ]])
+vim.cmd([[ let g:NERDTreeDirArrowCollapsible = '󰍟' ]])
---
+++ Gondor/config/nvim/lua/plugins/configs_plugins/telescope.lua
@@ -0,0 +1,5 @@
+--Find files using Telescope command-line sugar.
+vim.cmd([[ nnoremap <leader>ff <cmd>Telescope find_files<cr> ]])
+vim.cmd([[ nnoremap <leader>fg <cmd>Telescope live_grep<cr> ]])
+vim.cmd([[ nnoremap <leader>fb <cmd>Telescope buffers<cr> ]])
+vim.cmd([[ nnoremap <leader>fh <cmd>Telescope help_tags<cr> ]])
