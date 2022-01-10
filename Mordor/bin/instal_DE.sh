be7d18157d4a8f770dda2c1e6af3acfbd6359c75
José Antônio <jfreitas@unb.br>
2022-01-10 16:26:44 -0300

    Ajustes para uso na UnB.

--- Mordor/bin/instal_DE.sh
+++ Mordor/bin/instal_DE.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-git clone https://aur.archlinux.org/paru.git
-cd paru/
-makepkg -si --noconfirm
+git clone https://aur.archlinux.org/aura-bin.git
+cd aura-bin
+makepkg
+#sudo pacman -U <the-package-file-that-makepkg-produces>
 
 cd ..
 
-paru -S --noconfirm burpsuite candy-icons-git davinci-resolve dropbox google-chrome nerd-fonts-mononoki onedrive-abraunegg picom-jonaburg-git siji-git teams ttf-meslo ttf-ms-fonts ttf-unifont nerd-fonts-iosevka cpuid2cpuflags auto-cpufreq otf-font-awesome-5-free polybar ttf-material-design-icons-git
+sudo aura -A --noconfirm burpsuite candy-icons-git davinci-resolve dropbox google-chrome nerd-fonts-mononoki onedrive-abraunegg picom-jonaburg-git siji-git teams ttf-meslo ttf-ms-fonts ttf-unifont nerd-fonts-iosevka cpuid2cpuflags auto-cpufreq otf-font-awesome-5-free polybar ttf-material-design-icons-git
 
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-
-sudo pacman -Syy
-
-sleep 5
-
-sudo pacman -S --noconfirm xorg bspwm sxhkd firefox nitrogen lxappearance dmenu arandr arc-gtk-theme arc-icon-theme obs-studio vlc ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-hack ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji archlinux-wallpaper rofi playerctl scrot dunst pacman-contrib sublime-text cuda composer file-roller filezilla htop imagemagick img2pdf mousetweaks nodejs npm php numlockx pinta qalculate-gtk trayer vim-spell-pt volumeicon wget xdotool xmobar xmonad xmonad-contrib xournalpp youtube-dl vifm xorg-xinit pcmanfm keychain alacritty autorandr gitui networkmanager-openvpn nvidia-dkms ranger ripgrep the_silver_searcher ttf-liberation usbutils wmctrl xorg-xauth xorg-xmodmap xorg-xrdb
+sudo pacman -S --noconfirm xorg bspwm sxhkd firefox nitrogen lxappearance dmenu arandr arc-gtk-theme arc-icon-theme obs-studio vlc ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-hack ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji archlinux-wallpaper rofi playerctl scrot dunst pacman-contrib composer file-roller filezilla htop imagemagick img2pdf mousetweaks nodejs npm php numlockx pinta qalculate-gtk trayer vim-spell-pt volumeicon wget xdotool xmobar xmonad xmonad-contrib xournalpp youtube-dl vifm xorg-xinit pcmanfm keychain alacritty autorandr gitui ranger ripgrep the_silver_searcher ttf-liberation usbutils wmctrl xorg-xauth xorg-xmodmap xorg-xrdb
 
 
 # sudo systemctl enable lightdm
--- Mordor/bin/instal_arch_linux.sh
+++ Mordor/bin/instal_arch_linux.sh
@@ -6,10 +6,10 @@ sed -i '393s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=us-acentos" >> /etc/vconsole.conf
-echo "gondor" >> /etc/hostname
+echo "mordor" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 gondor.localdomain gondor" >> /etc/hosts
+echo "127.0.1.1 mordor.localdomain gondor" >> /etc/hosts
 echo root:password | chpasswd
 
 pacman -S --noconfirm reflector rsync
@@ -21,20 +21,18 @@ pacman -Syyy
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-lts-headers xdg-user-dirs xdg-utils gvfs nfs-utils inetutils dnsutils bash-completion openssh acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables ipset nss-mdns acpid os-prober ntfs-3g terminus-font awesome-terminal-fonts bat exa bpytop zsh libreoffice-fresh libreoffice-fresh-pt-br meld neofetch texinfo texlive-bibtexextra texlive-core texlive-fontsextra texlive-formatsextra texlive-latexextra texlive-pictures texlive-pstricks texlive-publishers texlive-science transmission-gtk ttf-fira-code ttf-fira-mono ttf-font-awesome zathura zathura-djvu zathura-pdf-mupdf zathura-ps audacity gimp gimp-help-pt_br keepassxc p7zip papirus-icon-theme pdftk python-beautifulsoup4 python-pip terminator alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol xorg-xinit fzf
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-lts-headers xdg-user-dirs xdg-utils gvfs nfs-utils inetutils dnsutils bash-completion openssh acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables ipset nss-mdns acpid terminus-font awesome-terminal-fonts bat exa bpytop zsh libreoffice-fresh libreoffice-fresh-pt-br meld neofetch texinfo texlive-bibtexextra texlive-core texlive-fontsextra texlive-formatsextra texlive-latexextra texlive-pictures texlive-pstricks texlive-publishers texlive-science transmission-gtk ttf-fira-code ttf-fira-mono ttf-font-awesome zathura zathura-djvu zathura-pdf-mupdf zathura-ps audacity gimp gimp-help-pt_br keepassxc p7zip papirus-icon-theme pdftk python-beautifulsoup4 python-pip terminator alsa-utils pulseaudio pulseaudio-alsa pavucontrol xorg-xinit fzf cups hplip
 
-# pacman -S --noconfirm xf86-video-amdgpu
-pacman -S --noconfirm nvidia-lts nvidia-utils nvidia-settings nvidia linux linux-headers
+# pacman -S --noconfirm xf86-video-amdgpu linux linux-headers
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 #systemctl enable bluetooth
-#systemctl enable cups.service
+systemctl enable cups.service
 systemctl enable sshd
 #systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
