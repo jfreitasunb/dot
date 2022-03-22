3f967a4dc6374ada704b7ba4fa900bac7dc0496d
José Antônio <jfreitas@unb.br>
2022-03-22 14:29:13 -0300

    Removendo pacotes que serão testados como appimage.

--- Mordor/bin/instal_arch_linux.sh
+++ Mordor/bin/instal_arch_linux.sh
@@ -14,14 +14,14 @@ echo root:password | chpasswd
 
 pacman -S --noconfirm reflector rsync
 
-reflector -c Brazil -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+reflector -c Brazil,'United States' -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 pacman -Syyy
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-lts-headers xdg-user-dirs xdg-utils gvfs nfs-utils inetutils dnsutils bash-completion openssh acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables ipset nss-mdns acpid terminus-font awesome-terminal-fonts bat exa bpytop zsh libreoffice-fresh libreoffice-fresh-pt-br meld neofetch texinfo texlive-bibtexextra texlive-core texlive-fontsextra texlive-formatsextra texlive-latexextra texlive-pictures texlive-pstricks texlive-publishers texlive-science transmission-gtk ttf-fira-code ttf-fira-mono ttf-font-awesome zathura zathura-djvu zathura-pdf-mupdf zathura-ps audacity gimp gimp-help-pt_br keepassxc p7zip papirus-icon-theme pdftk python-beautifulsoup4 python-pip terminator alsa-utils pulseaudio pulseaudio-alsa pavucontrol xorg-xinit fzf cups hplip
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools base-devel xdg-user-dirs xdg-utils gvfs nfs-utils inetutils dnsutils bash-completion openssh acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables ipset nss-mdns acpid terminus-font awesome-terminal-fonts bat exa bpytop zsh meld neofetch texinfo texlive-bibtexextra texlive-core texlive-fontsextra texlive-formatsextra texlive-latexextra texlive-pictures texlive-pstricks texlive-publishers texlive-science ttf-fira-code ttf-fira-mono ttf-font-awesome zathura zathura-djvu zathura-pdf-mupdf zathura-ps keepassxc p7zip papirus-icon-theme pdftk terminator alsa-utils pulseaudio pulseaudio-alsa pavucontrol xorg-xinit fzf cups hplip
 
 # pacman -S --noconfirm xf86-video-amdgpu linux linux-headers
 
