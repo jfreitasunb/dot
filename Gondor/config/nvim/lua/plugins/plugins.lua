3ca5f4dcd811c99d3d7c75ca65dde6914258f637
José Antônio <jfreitas@unb.br>
2022-04-25 19:23:19 -0300

    Retornado para as configurações antigas.

--- Gondor/config/nvim/lua/plugins/plugins.lua
+++ Gondor/config/nvim/lua/plugins/plugins.lua
@@ -6,24 +6,13 @@ end
 
 vim.cmd([[ packadd packer.nvim ]])
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
-
 return require('packer').startup(function(use)
-  use 'jalvesaq/Nvim-R'
   use 'wbthomason/packer.nvim'
   use 'morhetz/gruvbox'
   use 'tpope/vim-fugitive'
   use 'mattn/emmet-vim'
   use 'tpope/vim-sensible'
-  -- use {'preservim/nerdtree', config = [[ require('plugins/configs_plugins/nerdtree')]]}
-  use {'kyazdani42/nvim-tree.lua', config = [[ require('plugins/configs_plugins/treesitter') ]]}
-  use "akinsho/bufferline.nvim"
+  use {'preservim/nerdtree', config = [[ require('plugins/configs_plugins/nerdtree')]]}
   use 'norcalli/nvim-colorizer.lua'
   use 'tpope/vim-surround'
   use {'windwp/nvim-autopairs', config = [[ require('plugins/configs_plugins/autopairs') ]]}
@@ -39,26 +28,11 @@ return require('packer').startup(function(use)
       config = [[ require('plugins/configs_plugins/airline')]]
   }
   use {'bling/vim-bufferline', config = [[ require('plugins/configs_plugins/bufferline')]]}
---  use {
---      'neoclide/coc.nvim',
---      requires = { {'antoinemadec/coc-fzf'} },
---      config = [[ require('plugins/configs_plugins/coc') ]]
---    }
---
--- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
-
-  -- LSP
-  use {'neovim/nvim-lspconfig', config = [[ require('plugins/lsp/init')]]} -- enable LSP
-  use {'williamboman/nvim-lsp-installer', config = [[ require('plugins/lsp/lsp-installer') ]] } -- simple to use language server installer
-  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-
+  use {
+      'neoclide/coc.nvim',
+      requires = { {'antoinemadec/coc-fzf'} },
+      config = [[ require('plugins/configs_plugins/coc') ]]
+    }
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} },
