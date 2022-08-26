76ee8cbecefa8bce7f1ef0e1088de0c1e153f14a
José Antônio Oliveira de Freitas <jfreitas@unb.br>
2022-08-26 10:07:56 -0300

    Gerando o módulo para todos os kernels disponíveis.

--- Gondor/bin/teclado_avell.sh
+++ Gondor/bin/teclado_avell.sh
@@ -11,9 +11,13 @@ make
 
 sudo make install
 
-sudo install -m644 clevo-xsm-wmi.ko /lib/modules/$(uname -r)/extra
+for i in /lib/modules/*; do
+    if [ -d $i'/extra/' ]; then
+        sudo install -m644 clevo-xsm-wmi.ko $i'/extra'
 
-sudo depmod
+        sudo depmod
+    fi
+done
 
 sudo tee /etc/modprobe.d/clevo-xsm-wmi.conf <<< 'options clevo-xsm-wmi kb_color=red,red,red kb_brightness=1 kb_off=0'
 
