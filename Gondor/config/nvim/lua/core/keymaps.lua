2df3dc44dbc3b2f0e749977b58ca1d00fd10d775
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-04-04 14:12:20 -0300

    Leader key do neovim.

--- Gondor/config/nvim/lua/core/keymaps.lua
+++ Gondor/config/nvim/lua/core/keymaps.lua
@@ -10,8 +10,8 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Change leader to a comma
 --map("", "<Space>", "<Nop>", opts)
