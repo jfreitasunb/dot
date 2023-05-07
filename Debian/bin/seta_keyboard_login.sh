b4a805fca5d0dbec47a930247b17698c43f233bb
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-05-07 11:25:34 -0300

    Definindo o teclado no gnome.

--- Debian/bin/seta_keyboard_login.sh
+++ Debian/bin/seta_keyboard_login.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
+
 if [ "$(lsusb | grep -c '045e:0745')" -ge 1 ]; then
-    setxkbmap -model abnt2 -layout br -variant abnt2
+    gsettings set org.gnome.desktop.input-sources sources "[(('xkb', 'br')]"
 else
-   setxkbmap -layout us -variant intl 
+   gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+alt-intl')]"
 fi
