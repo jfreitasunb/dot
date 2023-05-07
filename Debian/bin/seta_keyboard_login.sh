2e60989bf095df1880f682b1a98473cd7a680bb4
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-05-07 11:52:29 -0300

    Ajustes.

--- Debian/bin/seta_keyboard_login.sh
+++ Debian/bin/seta_keyboard_login.sh
@@ -2,7 +2,7 @@
 
 
 if [ "$(lsusb | grep -c '045e:0745')" -ge 1 ]; then
-    gsettings set org.gnome.desktop.input-sources sources "[(('xkb', 'br')]"
+    gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'br')]"
 else
    gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+alt-intl')]"
 fi
