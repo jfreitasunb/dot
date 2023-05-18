cdb91f94c22199008e78f3bdc260fd9472deed87
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-05-18 15:51:37 -0300

    Teclado no Xmonad.

--- Debian/bin/seta_keyboard_login.sh
+++ Debian/bin/seta_keyboard_login.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
-
 if [ "$(lsusb | grep -c '045e:0745')" -ge 1 ]; then
-    gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'br')]"
+    setxkbmap -model abnt2 -layout br -variant abnt2
 else
-   gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+alt-intl')]"
+    setxkbmap -layout us -variant intl
 fi
