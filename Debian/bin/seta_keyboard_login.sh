f8e3bdae48a26e3d7d91d6194850ae293dc1f74a
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-05-22 20:51:09 -0300

    Testando para ver se agora funciona.

--- Debian/bin/seta_keyboard_login.sh
+++ Debian/bin/seta_keyboard_login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if [ "$(lsusb | grep -c '045e:0745')" -ge 1 ]; then
+    if [ "$(lsusb | grep -c '045e:0745')" -ge 0 ]; then
     setxkbmap -model abnt2 -layout br -variant abnt2
 else
     setxkbmap -layout us -variant intl
