ec3a31a42ac0847ea6c97efe9d291ccd9a2cd0f4
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-06-28 14:44:50 -0300

    Configurações para o Dropbox.

--- NixOS/UnB/configuration.nix
+++ NixOS/UnB/configuration.nix
@@ -144,6 +144,7 @@
     curl
     dmenu-rs
     dropbox
+    dropbox-cli
     exa
     feh
     flameshot
@@ -197,6 +198,30 @@
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
   
+  # Ativando o Dropbox
+  networking.firewall = {
+    allowedTCPPorts = [ 17500 ];
+    allowedUDPPorts = [ 17500 ];
+  };
+
+  systemd.user.services.dropbox = {
+    description = "Dropbox";
+    wantedBy = [ "graphical-session.target" ];
+    environment = {
+      QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+      QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
+      ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
+      KillMode = "control-group"; # upstream recommends process
+      Restart = "on-failure";
+      PrivateTmp = true;
+      ProtectSystem = "full";
+      Nice = 10;
+    };
+  };
+
   # Instalando fontes
   fonts = {
     fonts = with pkgs; [
