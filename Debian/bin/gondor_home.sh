93825abd889fc46ccf24a8ae0eb943d0bbb495c9
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-07-28 10:35:46 -0300

    Ajuste no script de backup.

--- Debian/bin/gondor_home.sh
+++ Debian/bin/gondor_home.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ultimo_backup=$(cat ~/.temporario/data_ultimo_backup)
-data_atual=$(date '+%s')
 
+data_atual=$(date '+%s')
 
 data_ultimo_backup=$(date -d $ultimo_backup '+%s')
 
@@ -13,24 +13,15 @@ rotavidade=3
 if [ $diferenca -gt $rotavidade ];
 then
 
-    EXCLUDE_LIST="/Arquivos/Dropbox/Backups/Gondor/excludes/exclude-HOME.list"
+    EXCLUDE_LIST="/Arquivos/Dropbox/Backups/Debian/excludes/exclude-HOME.list"
 
-    #HOME="/home/jfreitas/"
+    DEST_HOME="/Arquivos/Dropbox/Backups/Debian/HOME/"
 
-    #DEST_HOME_TEMP="/Arquivos/BACKUP-HOME-TEMP/jfreitas/"
+    NOME_BACKUP="debian_backup_diario_home-jfreitas_"$(date +%Y-%m-%d)".tar.bz2"
 
-    #DEST_TEMP="/Arquivos/BACKUP-HOME-TEMP/"
-
-    #mkdir -p $DEST_TEMP
-
-    DEST_HOME="/Arquivos/Dropbox/Backups/Gondor/HOME/"
-
-    NOME_BACKUP="gondor_backup_diario_home-jfreitas_"$(date +%Y-%m-%d)".tar.bz2"
-
-    #rsync -avzz --exclude-from="$EXCLUDE_LIST" "$HOME" "$DEST_HOME_TEMP"
     cd "$DEST_HOME"
 
-    tar --exclude-from="$EXCLUDE_LIST" -cjf - /home/jfreitas | split -d -b 1G - "$NOME_BACKUP""_parte-"
+    tar --exclude-from="$EXCLUDE_LIST" -cjf - /home/jfreitas | split -d -b 4G - "$NOME_BACKUP""_parte-"
 
     find "$DEST_HOME" -type f -mtime +10 -delete
 
