64c13ab7e3d66f148a6400b27008e4c6f54cc8bb
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-08-01 10:22:12 -0300

    Variáveis não utilizadas.

--- Debian/bin/gondor_root.sh
+++ Debian/bin/gondor_root.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-ROTATIVIDADE=10
-
 EXCLUDE_LIST="/Arquivos/Dropbox/Backups/Debian/excludes/exclude-debian.list"
 
 NOME_BACKUP="debian_backup_diario_root_"$(date +%Y-%m-%d)".tar.bz2"
 
-LOCAL_TEMPORARIO_ROOT="/Arquivos/Backup_Temporario/Debian/ROOT/"
-
-LOCAL_TAR="/Arquivos/Backup_Temporario/"
-
 LOCAL_BACKUP="/Arquivos/Dropbox/Backups/Debian/Backup-Diario/"
-
-#rsync -avzzc --links --delete --exclude-from="$EXCLUDE_LIST" /etc "$LOCAL_TEMPORARIO_ROOT"
-
-#cd "$LOCAL_BACKUP"
 
 tar --exclude-from="$EXCLUDE_LIST" -cjf "$LOCAL_BACKUP""$NOME_BACKUP" /etc
 
 find "$LOCAL_BACKUP""$NOME_BACKUP" -type f -mtime +10 -delete
 
-# rsync -avzc --no-links --delete --exclude-from=$EXCLUDE_LIST /var/lib/vnstat $LOCAL_TEMPORARIO_ROOT
-
-#$cd "$LOCAL_TAR"
-
-#tar -cvjf "$LOCAL_TAR""$NOME_BACKUP" "$LOCAL_TEMPORARIO_ROOT"
-
-#cp "$NOME_BACKUP" "$LOCAL_BACKUP"
-
 chown -R jfreitas:jfreitas "$LOCAL_BACKUP"
-
-#rm "$NOME_BACKUP"
