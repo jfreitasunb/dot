c0bf32804ab3951b8d6782eec5fe832a69635ae3
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-08-01 10:14:51 -0300

    Coisas não utilizadas.

--- Debian/bin/gondor_lista_pacotes.sh
+++ Debian/bin/gondor_lista_pacotes.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# ROTATIVIDADE=10
-
-# NOME_BACKUP_APT="debian_lista_pacotes_instalados_APT_"$(date +%Y-%m-%d)".lst"
-
 NOME_BACKUP_GNOME="debian_gnome_settings_"$(date +%Y-%m-%d)".ini"
 
 NOME_BACKUP_APT="debian_lista_pacotes_instalados_APT_"$(date +%Y-%m-%d)".lst"
@@ -11,12 +7,6 @@ NOME_BACKUP_APT="debian_lista_pacotes_instalados_APT_"$(date +%Y-%m-%d)".lst"
 LOCAL_BACKUP="/Arquivos/Dropbox/Backups/Debian/Backup-Diario/"
 
 dpkg-query -f '${binary:Package}\n' -W > $LOCAL_BACKUP$NOME_BACKUP
-
-#pacman -Qqe | grep -v "$(pacman -Qqm)" > "$LOCAL_BACKUP""$NOME_BACKUP_APT"
-
-#pacman -Qqm > "$LOCAL_BACKUP""$NOME_BACKUP_PARU"
-
-#sed -i '/paru/d' "$LOCAL_BACKUP""$NOME_BACKUP_PARU"
 
 find "$LOCAL_BACKUP" -type f -mtime +10 -delete
 
@@ -27,6 +17,4 @@ find "$LOCAL_BACKUP" -type f -mtime +10 -delete
 #dconf load / < dconf-settings.ini
 
 #Para reinstalar
-#yay -S - < ~/debian_lista_pacotes_instalados_YAY.lst
-#pacman -S --needed - < ~/debian_lista_pacotes_instalados_APT.lst
 #sudo xargs -a packages_list.txt apt install
