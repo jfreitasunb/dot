37d2c67581d55462f018595ed1f2a12b397f752c
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-08-01 10:19:24 -0300

    Nomes das variáveis.

--- Debian/bin/gondor_root.sh
+++ Debian/bin/gondor_root.sh
@@ -2,15 +2,15 @@
 
 ROTATIVIDADE=10
 
-EXCLUDE_LIST="/Arquivos/Dropbox/Backups/Gondor/excludes/exclude-gondor.list"
+EXCLUDE_LIST="/Arquivos/Dropbox/Backups/Debian/excludes/exclude-debian.list"
 
-NOME_BACKUP="gondor_backup_diario_root_"$(date +%Y-%m-%d)".tar.bz2"
+NOME_BACKUP="debian_backup_diario_root_"$(date +%Y-%m-%d)".tar.bz2"
 
-LOCAL_TEMPORARIO_ROOT="/Arquivos/Backup_Temporario/Gondor/ROOT/"
+LOCAL_TEMPORARIO_ROOT="/Arquivos/Backup_Temporario/Debian/ROOT/"
 
 LOCAL_TAR="/Arquivos/Backup_Temporario/"
 
-LOCAL_BACKUP="/Arquivos/Dropbox/Backups/Gondor/Backup-Diario/"
+LOCAL_BACKUP="/Arquivos/Dropbox/Backups/Debian/Backup-Diario/"
 
 #rsync -avzzc --links --delete --exclude-from="$EXCLUDE_LIST" /etc "$LOCAL_TEMPORARIO_ROOT"
 
