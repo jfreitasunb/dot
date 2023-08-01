538e6ee5f42778cdf7a52c731b5d939188fe7272
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-08-01 14:36:10 -0300

    Local correto para salvar os arquivos.

--- Debian/bin/gondor_lista_pacotes.sh
+++ Debian/bin/gondor_lista_pacotes.sh
@@ -2,11 +2,11 @@
 
 NOME_BACKUP_GNOME="debian_gnome_settings_"$(date +%Y-%m-%d)".ini"
 
-NOME_BACKUP_APT="debian_lista_pacotes_instalados_APT_"$(date +%Y-%m-%d)".lst"
+NOME_BACKUP="debian_lista_pacotes_instalados_APT_"$(date +%Y-%m-%d)".lst"
 
 LOCAL_BACKUP="/Arquivos/Dropbox/Backups/Debian/Backup-Diario/"
 
-dpkg-query -f '${binary:Package}\n' -W > $LOCAL_BACKUP$NOME_BACKUP
+dpkg-query -f '${binary:Package}\n' -W > "$LOCAL_BACKUP""$NOME_BACKUP"
 
 find "$LOCAL_BACKUP" -type f -mtime +10 -delete
 
