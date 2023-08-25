ef242595dbbe12486f7a42e0cf6b0ba50e90aadf
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-08-25 10:33:20 -0300

    Deixando por último.

--- Debian/bin/instala_debian.sh
+++ Debian/bin/instala_debian.sh
@@ -127,8 +127,6 @@ ln -s GitHub/dot/Gondor/config/latexmkrc ./.latexmkrc
 
 ln -s GitHub/dot/Debian/config/Xmodmap ./.Xmodmap
 
-sudo usermod -aG libvirt jfreitas
-
 cd /tmp
 
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
@@ -138,3 +136,5 @@ zcat install-tl-unx.tar.gz | tar xf -
 cd install-tl-*/
 
 sudo perl ./install-tl --gui
+
+sudo usermod -aG libvirt jfreitas
