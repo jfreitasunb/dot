9fd8b7e0fc9b46864501bbc99244afdbc43d7986
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-09-29 15:43:28 -0300

    Zplug e configurações.

--- Arch/bin/instal_DE.sh
+++ Arch/bin/instal_DE.sh
@@ -12,7 +12,7 @@ cd ..
 
 rm -rf paru
 
-paru -S burpsuite candy-icons-git picom siji-git ttf-meslo ttf-ms-fonts ttf-unifont auto-cpufreq otf-font-awesome-5 ttf-material-design-icons-git brave-bin
+paru -S burpsuite candy-icons-git picom siji-git ttf-meslo ttf-ms-fonts ttf-unifont auto-cpufreq otf-font-awesome-5 ttf-material-design-icons-git brave-bin zplug
 
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 
@@ -98,5 +98,52 @@ sudo make install
 cd ~
 
 rm -rf nvidia
+
+cd ~/.config/
+
+ln -s ~/GitHub/dot/Arch/config/alacritty ./
+
+ln -s ~/GitHub/dot/Arch/config/aliases/ ./
+
+ln -s ~/GitHub/dot/Arch/config/autorandr/ ./
+
+ln -s ~/GitHub/dot/Arch/config/nvim/ ./
+
+ln -s ~/GitHub/dot/Arch/config/picom/ ./
+
+ln -s ~/GitHub/dot/Arch/config/terminator/ ./
+
+ln -s ~/GitHub/dot/Arch/config/xmonad/ ./
+
+ln -s ~/GitHub/dot/Arch/config/xmobar/ ./
+
+ln -s ~/GitHub/dot/Arch/config/zathura/ ./
+
+ln -s ~/GitHub/dot/Arch/config/zsh/ ./
+
+ln -s ~/GitHub/dot/Arch/config/keepassxc/ ./
+
+ln -s ~/GitHub/dot/Arch/config/gtk-3.0 ./
+
+mkdir texstudio
+
+cd texstudio
+
+ln -s ~/GitHub/dot/Arch/config/texstudio/texstudio.ini ./
+
+cd ~
+
+ln -s ~/GitHub/dot/Arch/bin/ ./.local/bin
+
+cd .local/share/
+
+ln -s ~/GitHub/dot/Arch/fonts/ ./
+
+fc-cache -f -v
+
+ln -s GitHub/dot/Gondor/config/latexmkrc ./.latexmkrc
+
+ln -s GitHub/dot/Arch/config/Xmodmap ./.Xmodmap
+
 
 printf "\e[1;32mCHANGE NECESSARY FILES BEFORE REBOOT\e[0m"
