e739b6c95c2f138924696ce1a54f3aeabd781cbe
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-10-23 14:28:17 -0300

    Pacotes que não estão disponíveis no Manjaro pelo pacman.

--- Manjaro/bin/instal_DE.sh
+++ Manjaro/bin/instal_DE.sh
@@ -22,14 +22,14 @@ sudo pacman -Syy
 
 sleep 5
 
-sudo pacman -S --needed xorg firefox nitrogen lxappearance dmenu arandr arc-gtk-theme arc-icon-theme vlc \
+sudo pacman -S --needed firefox nitrogen lxappearance dmenu arandr arc-gtk-theme arc-icon-theme vlc \
   ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts \
   ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-hack ttf-inconsolata \
   ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font \
-  adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji archlinux-wallpaper rofi playerctl \
+  adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji rofi playerctl \
   scrot dunst pacman-contrib cuda composer file-roller filezilla htop imagemagick img2pdf mousetweaks nodejs npm php \
-  numlockx pinta qalculate-gtk trayer vim-spell-pt volumeicon wget xdotool xmobar xmonad xmonad-contrib xournalpp youtube-dl vifm \
-  xorg-xinit pcmanfm keychain alacritty autorandr nvidia-dkms ranger ripgrep the_silver_searcher \
+  numlockx pinta qalculate-gtk trayer vim-spell-pt volumeicon wget xdotool xmobar xmonad xmonad-contrib xournalpp vifm \
+  xorg-xinit pcmanfm keychain alacritty autorandr ranger ripgrep the_silver_searcher \
   ttf-liberation usbutils wmctrl xorg-xauth xorg-xmodmap xorg-xrdb vlc sublime-text
 
 
