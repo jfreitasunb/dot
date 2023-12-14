3cfb2d5a3190d97eb883f40905400a89380f5588
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-12-14 18:16:36 -0300

    Nome do arquivo de backup.

--- Arch/bin/gondor_lista_pacotes.sh
+++ Arch/bin/gondor_lista_pacotes.sh
@@ -10,7 +10,7 @@ NOME_BACKUP_GNOME="gondor_gnome_settings_"$(date +%Y-%m-%d)".ini"
 #
 NOME_BACKUP_PACMAN="gondor_lista_pacotes_instalados_PACMAN_"$(date +%Y-%m-%d)".lst"
 
-NOME_BACKUP_PARU="gondor_lista_pacotes_instalados_PARU_"$(date +%Y-%m-%d)".lst"
+NOME_BACKUP_PARU="gondor_lista_pacotes_instalados_YAY_"$(date +%Y-%m-%d)".lst"
 
 LOCAL_BACKUP="/Arquivos/Dropbox/Backups/Gondor/Backup-Diario/"
 
@@ -20,7 +20,7 @@ pacman -Qqe | grep -v "$(pacman -Qqm)" > "$LOCAL_BACKUP""$NOME_BACKUP_PACMAN"
 
 pacman -Qqm > "$LOCAL_BACKUP""$NOME_BACKUP_PARU"
 
-sed -i '/paru/d' "$LOCAL_BACKUP""$NOME_BACKUP_PARU"
+sed -i '/yay/d' "$LOCAL_BACKUP""$NOME_BACKUP_PARU"
 
 find "$LOCAL_BACKUP" -type f -mtime +10 -delete
 
