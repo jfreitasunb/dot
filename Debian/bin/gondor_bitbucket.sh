b447a312a71acbf693d25534a84c7175b940ba51
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-03-02 10:49:39 -0300

    Novo caminho dos arquivos.

--- Debian/bin/gondor_bitbucket.sh
+++ Debian/bin/gondor_bitbucket.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
-EXCLUDE_LIST_TEX="/Arquivos/Dropbox/Backups/Debian/excludes/exclude-TEX.list"
+EXCLUDE_LIST_TEX="/Arquivos/OneDriveUnB/Backups/Debian/excludes/exclude-TEX.list"
 
-EXCLUDE_LIST_LARAVEL="/Arquivos/Dropbox/Backups/Debian/excludes/exclude-GitHub_laravel.list"
+EXCLUDE_LIST_LARAVEL="/Arquivos/OneDriveUnB/Backups/Debian/excludes/exclude-GitHub_laravel.list"
 
 ALGEBRA1="/home/jfreitas/GitHub/Algebra-1/"
-DEST_ALGEBRA1="/Arquivos/Dropbox/UnB/Disciplinas/Graduacao/Algebra_1/2024-1"
+DEST_ALGEBRA1="/Arquivos/OneDriveUnB/UnB/Disciplinas/Graduacao/Algebra_1/2024-1"
 
 #IAL="/home/jfreitas/GitHub/IAL/"
-#DEST_IAL="/Arquivos/Dropbox/UnB/Disciplinas/Graduacao/IAL/2023-2"
+#DEST_IAL="/Arquivos/OneDriveUnB/UnB/Disciplinas/Graduacao/IAL/2023-2"
 
 #AULASONLINE="/home/jfreitas/GitHub/video_aulas/"
-#DEST_AULASONLINE="/Arquivos/Dropbox/UnB/Disciplinas/Graduacao/pdf_video_aulas"
+#DEST_AULASONLINE="/Arquivos/OneDriveUnB/UnB/Disciplinas/Graduacao/pdf_video_aulas"
 
 #ALGEBRA_LINEAR="/home/jfreitas/GitHub/algebra_linear/"
-#DEST_ALGEBRA_LINEAR="/Arquivos/Dropbox/UnB/Disciplinas/Graduacao/Algebra_Linear/2019-1"
+#DEST_ALGEBRA_LINEAR="/Arquivos/OneDriveUnB/UnB/Disciplinas/Graduacao/Algebra_Linear/2019-1"
 
 #CAT="/home/jfreitas/GitHub/cat/"
-#DEST_CAT="/Arquivos/Dropbox/UnB/CAT"
+#DEST_CAT="/Arquivos/OneDriveUnB/UnB/CAT"
 
 #MONITORIAMAT="/home/jfreitas/GitHub/inscricoesmonitoria/"
-#DEST_MONITORIAMAT="/Arquivos/Dropbox/UnB/Projetos-PHP/inscricoesmonitoria"
+#DEST_MONITORIAMAT="/Arquivos/OneDriveUnB/UnB/Projetos-PHP/inscricoesmonitoria"
 
 #INSCRICOESPOSMAT="/home/jfreitas/GitHub/inscricoespos/"                  
-#DEST_INSCRICOESPOSMAT="/Arquivos/Dropbox/UnB/Projetos-PHP/inscricoespos"
+#DEST_INSCRICOESPOSMAT="/Arquivos/OneDriveUnB/UnB/Projetos-PHP/inscricoespos"
 
 #POSMAT="/home/jfreitas/GitHub/inscricoespos/"
-#DEST_POSMAT="/Arquivos/Dropbox/UnB/Projetos-PHP/inscricoespos"
+#DEST_POSMAT="/Arquivos/OneDriveUnB/UnB/Projetos-PHP/inscricoespos"
 
 #INSCRICOESPNPD="/home/jfreitas/GitHub/inscricoespnpd/"
-#DEST_INSCRICOESPNPD="/Arquivos/Dropbox/UnB/Projetos-PHP/inscricoespnpd"
+#DEST_INSCRICOESPNPD="/Arquivos/OneDriveUnB/UnB/Projetos-PHP/inscricoespnpd"
 
 #PROFICIENCIA="/home/jfreitas/GitHub/proficiencia/"
-#DEST_PROFICIENCIA="/Arquivos/Dropbox/UnB/Projetos-PHP/proficiencia"
+#DEST_PROFICIENCIA="/Arquivos/OneDriveUnB/UnB/Projetos-PHP/proficiencia"
 
 #SITEMONITORIAMAT="/home/jfreitas/GitHub/site-monitoriamat/"
-#DEST_SITEMONITORIAMAT="/Arquivos/Dropbox/UnB/Projetos-PHP/site-monitoriamat"
+#DEST_SITEMONITORIAMAT="/Arquivos/OneDriveUnB/UnB/Projetos-PHP/site-monitoriamat"
 
 #INSCRICOESVERAO="/home/jfreitas/GitHub/inscricoesverao/"
-#DEST_INSCRICOESVERAO="/Arquivos/Dropbox/UnB/Projetos-PHP/inscricoesverao"
+#DEST_INSCRICOESVERAO="/Arquivos/OneDriveUnB/UnB/Projetos-PHP/inscricoesverao"
 
 #INSCRICOESEVENTOSMAT="/home/jfreitas/GitHub/inscricoeseventos/"
-#DEST_INSCRICOESEVENTOSMAT="/Arquivos/Dropbox/UnB/Projetos-PHP/inscricoeseventos"
+#DEST_INSCRICOESEVENTOSMAT="/Arquivos/OneDriveUnB/UnB/Projetos-PHP/inscricoeseventos"
 
 rsync -avzz --exclude-from="$EXCLUDE_LIST_TEX" "$ALGEBRA1" "$DEST_ALGEBRA1"
 
