045966b33083888136f68ee80433dd7b6489ebe1
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-03-14 17:25:30 -0300

    Coisas.

--- Debian/bin/wallpaper_gnome.sh
+++ Debian/bin/wallpaper_gnome.sh
@@ -2,7 +2,7 @@
 # script that changes me GNOME wallpapers in random order
 
 # setting the base path
-export wallpaper_path=/Arquivos/OneDrive/Wallpapers
+export wallpaper_path=/Arquivos/OneDrive/Pictures/Wallpapers
 shopt -s nullglob
 
 # storing images in an array
@@ -20,9 +20,9 @@ do
     # generating a random index
     random_index=$(($RANDOM % $wallpapers_size))
     # setting the random wallpaper
-    gsettings set org.gnome.desktop.background picture-uri-dark file://${wallpapers[$random_index]}
+    #gsettings set org.gnome.desktop.background picture-uri-dark file://${wallpapers[$random_index]}
     # trocar pelo comando abaixo se não estiver usando o tema escuro
-    # gsettings set org.gnome.desktop.background picture-uri file://${wallpapers[$random_index]}
+    gsettings set org.gnome.desktop.background picture-uri file://${wallpapers[$random_index]}
     # keeping it active for set time
     sleep 1 && exit
 done
