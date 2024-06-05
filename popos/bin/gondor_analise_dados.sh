ff88f985812ac03ae448032c1874f154ecdd69bd
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-06-04 21:18:56 -0300

    Configurações não utilizadas.

--- popos/bin/gondor_analise_dados.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-EXCLUDE_LIST_DADOS="/Arquivos/Dropbox/Backups/Gondor/excludes/exclude-analise_dados.list"
-
-DEST_ANALISEDADOS="/Arquivos/Dropbox/"
-
-ANALISEDADOS="/home/jfreitas/analise_de_dados"
-
-rsync -avzz --exclude-from="$EXCLUDE_LIST_DADOS" "$ANALISEDADOS" "$DEST_ANALISEDADOS"
