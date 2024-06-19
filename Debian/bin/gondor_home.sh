8596568fe48ebe4601c5acc6e463824e776f8149
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-06-19 20:30:20 -0300

    Pasta para backup.

--- Debian/bin/gondor_home.sh
+++ Debian/bin/gondor_home.sh
@@ -13,11 +13,11 @@ rotavidade=7
 if [ $diferenca -gt $rotavidade ];
 then
 
-    EXCLUDE_LIST="/home/jfreitas/OneDrive/Backups/Manjaro/excludes/exclude-HOME.list"
+    EXCLUDE_LIST="/home/jfreitas/OneDrive/Backups/Debian/excludes/exclude-HOME.list"
 
-    DEST_HOME="/home/jfreitas/OneDrive/Backups/Manjaro/HOME/"
+    DEST_HOME="/home/jfreitas/OneDrive/Backups/Debian/HOME/"
 
-    NOME_BACKUP="manjaro_backup_diario_home-jfreitas_"$(date +%Y-%m-%d)".tar.bz2"
+    NOME_BACKUP="debian_backup_diario_home-jfreitas_"$(date +%Y-%m-%d)".tar.bz2"
 
     cd "$DEST_HOME"
 
