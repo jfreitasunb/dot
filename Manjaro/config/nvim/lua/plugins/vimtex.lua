eac78a58fcb0d1667f02c8aa93b524041a815a70
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-09-20 16:08:13 -0300

    Mais coisas.

--- Manjaro/config/nvim/lua/plugins/vimtex.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd([[ let g:tex_flavor = "latex"]] )
-vim.cmd([[ let g:vimtex_quickfix_open_on_warning = 0]] )
-vim.cmd([[ let g:vimtex_compiler_progname = 'nvr']] )
-vim.cmd([[ let g:vimtex_view_method = 'zathura']] )
-vim.cmd([[ let g:vimtex_quickfix_ignore_filters = [ 'Underfull', 'Overfull', 'specifier changed to',] ]])
