ad3b68d431b511330a4910c7f97f91509289a91a
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-10-03 10:45:27 -0300

    Ajustes no script.

--- Arch/bin/instal_arch_linux.sh
+++ Arch/bin/instal_arch_linux.sh
@@ -2,7 +2,7 @@
 
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
-sed -i '387s/.//' /etc/locale.gen
+sed -i '391s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
@@ -18,9 +18,10 @@ reflector -c Brazil -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 pacman -Syyy
 
-pacman -S acpi acpi_call acpid alsa-utils awesome-terminal-fonts base-devel bash-completion bat bpytop bridge-utils cmake curl dialog dnsmasq dnsutils dosfstools ebtables edk2-ovmf efibootmgr grub gvfs inetutils ipset iptables-nft less libvirt linux-lts-headers mtools network-manager-applet networkmanager nfs-utils nss-mdns ntfs-3g openbsd-netcat openssh os-prober pacman-contrib p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse python-beautifulsoup4 python-pip qemu-full terminus-font tlp ttf-fira-code ttf-fira-mono ttf-font-awesome unzip vde2 virt-manager wget wpa_supplicant xdg-user-dirs xdg-utils xorg-xinit zsh
+pacman -S acpi acpi_call acpid alsa-utils awesome-terminal-fonts base-devel bash-completion bat bpytop bridge-utils cmake curl dialog dnsmasq dnsutils dosfstools ebtables edk2-ovmf efibootmgr grub gvfs inetutils ipset iptables-nft less libvirt linux-lts-headers mtools network-manager-applet networkmanager nfs-utils nss-mdns ntfs-3g openbsd-netcat openssh os-prober pacman-contrib p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse python-beautifulsoup4 python-pip qemu-full terminus-font tlp ttf-fira-code ttf-fira-mono ttf-font-awesome unzip vde2 virt-manager wget wpa_supplicant xdg-user-dirs xdg-utils xorg-xinit zsh bluez bluez-utils
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
