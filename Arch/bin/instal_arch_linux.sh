21f53b09ea5684bfc6919c767b2de1b560ccdded
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-10-17 11:09:10 -0300

    Correções.

--- Arch/bin/instal_arch_linux.sh
+++ Arch/bin/instal_arch_linux.sh
@@ -6,10 +6,10 @@ sed -i '391s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
-echo "gondor" >> /etc/hostname
+echo "rivendel" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 gondor.localdomain gondor" >> /etc/hosts
+echo "127.0.1.1 rivendel.localdomain rivendel" >> /etc/hosts
 echo root:password | chpasswd
 
 pacman -S reflector rsync
@@ -18,7 +18,7 @@ reflector -c Brazil -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 pacman -Syyy
 
-pacman -S acpi acpi_call acpid alsa-utils awesome-terminal-fonts base-devel bash-completion bat bpytop bridge-utils cmake curl dialog dnsmasq dnsutils dosfstools ebtables edk2-ovmf efibootmgr grub gvfs inetutils ipset iptables-nft less libvirt linux-lts-headers mtools network-manager-applet networkmanager nfs-utils nss-mdns ntfs-3g openbsd-netcat openssh os-prober pacman-contrib p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse python-beautifulsoup4 python-pip qemu-full terminus-font tlp ttf-fira-code ttf-fira-mono ttf-font-awesome unzip vde2 virt-manager wget wpa_supplicant xdg-user-dirs xdg-utils xorg-xinit zsh bluez bluez-utils
+pacman -S acpi acpi_call acpid alsa-utils awesome-terminal-fonts base-devel bash-completion bat bpytop bridge-utils cmake curl dialog dnsmasq dnsutils dosfstools ebtables edk2-ovmf efibootmgr grub gvfs inetutils ipset iptables-nft less libvirt linux-lts-headers mtools network-manager-applet networkmanager nfs-utils nss-mdns ntfs-3g openbsd-netcat openssh os-prober pacman-contrib p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse python-beautifulsoup4 python-pip qemu-full terminus-font tlp ttf-fira-code ttf-fira-mono ttf-font-awesome unzip vde2 virt-manager wget wpa_supplicant xdg-user-dirs xdg-utils xorg-xinit zsh bluez bluez-utils grub-btrfs
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 
