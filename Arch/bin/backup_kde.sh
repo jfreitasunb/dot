5f0f51c9c7dbc39018d5420b86672f394b368c52
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-12-19 16:46:27 -0300

    Adicionando arquivos no git.

--- Arch/bin/backup_kde.sh
+++ Arch/bin/backup_kde.sh
@@ -10,19 +10,6 @@ cp -R backup_$data_hoje/* /home/jfreitas/GitHub/dot/Arch/KDE-configs/
 
 cd /home/jfreitas/GitHub/dot/Arch/KDE-configs/
 
-git commit -am "Ajustes nas configurações do KDE feitas no dia "$data_hoje
+git add .
 
-# EXCLUDE_LIST="/home/jfreitas/OneDrive/Backups/Arch/excludes/exclude-HOME.list"
-
-# DEST_HOME="/home/jfreitas/OneDrive/Backups/Arch/HOME/"
-
-# NOME_BACKUP="arch_backup_diario_home-jfreitas_"$data_hoje".tar.bz2"
-
-# cd "$DEST_HOME"
-
-# tar --exclude-from="$EXCLUDE_LIST" -cjf - /home/jfreitas | split -d -b 4G - "$NOME_BACKUP""_parte-"
-
-# find "$DEST_HOME" -type f -mtime +10 -delete
-
-# echo $(date '+%Y-%m-%d') > ~/.temporario/data_ultimo_backup
-
+git commit -m "Ajustes nas configurações do KDE feitas no dia "$data_hoje
