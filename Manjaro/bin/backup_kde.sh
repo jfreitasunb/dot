519d113b985b41a548d666d032c366de4f00469d
José Antônio <jfreitas@unb.br>
2025-01-11 10:15:11 -0300

    Arquivos errados

--- Manjaro/bin/backup_kde.sh
+++ Manjaro/bin/backup_kde.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-data_hoje=$(date +%d-%m-%Y_%H:%m:%S)
+data_hoje=`date '+%d-%m-%Y_%H:%M:%S'`
 
 konsave -s backup_$data_hoje
 
-cp -R ~/.config/konsave /home/jfreitas/GitHub/dot/Manjaro/KDE-configs/
+cd ~/.config/konsave/profiles/
+
+cp -R backup_$data_hoje/* /home/jfreitas/GitHub/dot/Manjaro/KDE-configs/
 
 cd /home/jfreitas/GitHub/dot/Manjaro/KDE-configs/
 
