84b873fba895c323fe4ae6f6f88b252eb1220232
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-01-23 16:38:51 -0300

    Instalando o yazi.

--- Debian/bin/instala_debian.sh
+++ Debian/bin/instala_debian.sh
@@ -57,9 +57,23 @@ rm -rf dlang
 
 rm -rf onedrive
 
-curl https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 source "$HOME/.cargo/env"
+
+rustup update
+
+git clone https://github.com/sxyazi/yazi.git
+
+cd yazi
+
+cargo build --release --locked
+
+sudo mv target/release/yazi target/release/ya /usr/local/bin/
+
+cd ~
+
+rm -rf yazi
 
 # git clone https://github.com/alacritty/alacritty.git
 
