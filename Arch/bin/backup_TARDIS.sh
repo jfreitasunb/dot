06e5e02267c4574be1a8dd7786b2304b7527c6cf
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-02-26 21:17:12 -0300

    Mais algumas coisas.

--- Arch/bin/backup_TARDIS.sh
+++ Arch/bin/backup_TARDIS.sh
@@ -50,7 +50,7 @@ then
 
     EXCLUDE_LIST_TARDIS="/home/jfreitas/OneDrive/Backups/Tardis/excludes/exclude-Tardis.list"
 
-    rsync -avzzc --exclude-from="$EXCLUDE_LIST_TARDIS" "$BACKUP_SOURCE" "$BACKUP_DESTINATION"
+    rsync -avzzcu --exclude-from="$EXCLUDE_LIST_TARDIS" "$BACKUP_SOURCE" "$BACKUP_DESTINATION"
 
     umount $BACKUP_DEVICE
 
