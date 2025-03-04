b0db44c77f839a14d3455930fb70eed5e20554dd
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-03-04 16:40:08 -0300

    Correções.

--- Debian/bin/backup_TARDIS.sh
+++ Debian/bin/backup_TARDIS.sh
@@ -12,52 +12,47 @@ data_atual=$(date '+%s')
 
 ultimo_backup_tardis=$(date -d $ultimo_backup_tardis '+%s')
 
-diferenca=$(( ( data_atual - ultimo_backup_tardis )/(60*60*24) ))
+diferenca=$(((data_atual - ultimo_backup_tardis) / (60 * 60 * 24)))
 
 rotavidade=3
 
-if [ $diferenca -gt $rotavidade ];
-then
-  if [ -d "$BACKUP_DESTINATION" ] ; then
+if [ $diferenca -gt $rotavidade ]; then
+    if [ -d "$BACKUP_DESTINATION" ]; then
 
-    if [ -f $PIDFILE ]
-    then
-      PID=$(cat $PIDFILE)
-      ps -p $PID > /dev/null 2>&1
-      if [ $? -eq 0 ]
-      then
-        exit 1
-      else
-        ## Process not found assume not running
-        echo $$ > $PIDFILE
-        if [ $? -ne 0 ]
-        then
-            exit 1
+        if [ -f $PIDFILE ]; then
+            PID=$(cat $PIDFILE)
+            ps -p $PID >/dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                exit 1
+            else
+                ## Process not found assume not running
+                echo $$ >$PIDFILE
+                if [ $? -ne 0 ]; then
+                    exit 1
+                fi
+            fi
+        else
+            echo $$ >$PIDFILE
+            if [ $? -ne 0 ]; then
+                exit 1
+            fi
         fi
-      fi
-    else
-      echo $$ > $PIDFILE
-      if [ $? -ne 0 ]
-      then
-        exit 1
-      fi
+
+        notify-send "Backup iniciado. Não remova o HD Tardis."
+
+        BACKUP_SOURCE="/home/jfreitas/"
+
+        EXCLUDE_LIST_TARDIS="/home/jfreitas/OneDrive/Backups/Tardis/excludes/exclude-Tardis.list"
+
+        rsync -avzzuc --ignore-existing --delete --exclude-from="$EXCLUDE_LIST_TARDIS" "$BACKUP_SOURCE" "$BACKUP_DESTINATION"
+
+        umount $BACKUP_DEVICE
+
+        rm $PIDFILE
+
+        notify-send "Backup finalizado. Pode remover o HD Tardis."
+
+        echo $(date '+%Y-%m-%d') >~/.temporario/data_ultimo_backup_tardis
     fi
-
-    notify-send "Backup iniciado. Não remova o HD Tardis."
-
-    BACKUP_SOURCE="/home/jfreitas/"
-
-    EXCLUDE_LIST_TARDIS="/home/jfreitas/OneDrive/Backups/Tardis/excludes/exclude-Tardis.list"
-
-    rsync -avzzc --ignore-existing --delete --exclude-from="$EXCLUDE_LIST_TARDIS" "$BACKUP_SOURCE" "$BACKUP_DESTINATION"
-
-    umount $BACKUP_DEVICE
-
-    rm $PIDFILE
-
-    notify-send "Backup finalizado. Pode remover o HD Tardis."
-
-    echo $(date '+%Y-%m-%d') > ~/.temporario/data_ultimo_backup_tardis
-  fi
 
 fi
