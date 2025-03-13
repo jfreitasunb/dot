df0ba2da2f195eb91392d2577bdb0f9512a9b154
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-03-13 14:16:27 -0300

    Ajuste de opacidade.

--- Debian/stow_configs/wezterm/.config/wezterm/wezterm.lua
+++ Debian/stow_configs/wezterm/.config/wezterm/wezterm.lua
@@ -7,7 +7,6 @@ return {
 	font_size = 12.0,
 	font = wezterm.font("JetBrains Mono"),
 	-- macos_window_background_blur = 40,
-	macos_window_background_blur = 30,
 
 	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
 	-- window_background_image_hsb = {
@@ -15,8 +14,7 @@ return {
 	-- 	hue = 1.0,
 	-- 	saturation = 0.5,
 	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1.0,
+	window_background_opacity = 0.9,
 	-- window_background_opacity = 0.78,
 	-- window_background_opacity = 0.20,
 	window_decorations = "RESIZE",
