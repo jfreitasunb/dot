7ffc56050c9c84b27c8e9b13ceeab10468275e5a
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-03-19 21:05:57 -0300

    Alguns programas no formato flatpak.

--- Fedora/bin/instal_fedora.sh
+++ Fedora/bin/instal_fedora.sh
@@ -6,12 +6,13 @@ sudo dnf install dnf-plugins-core
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
 
-
-
-
 sudo dnf install brave-browser
 
 
 flatpak install flathub io.github.shiftey.Desktop
 
 flatpak install flathub com.spotify.Client
+
+flatpak install flathub com.bitwarden.desktop
+
+flatpak install flathub org.texstudio.TeXstudio
