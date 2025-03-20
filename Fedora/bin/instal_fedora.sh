c6c73598175f1af24e004023cfa29328f317ede5
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-03-19 21:08:50 -0300

    Chave GPG e repositório do Sublime Text.

--- Fedora/bin/instal_fedora.sh
+++ Fedora/bin/instal_fedora.sh
@@ -5,6 +5,9 @@ sudo dnf install dnf-plugins-core
 
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+
+sudo dnf config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
 sudo dnf install brave-browser
 
