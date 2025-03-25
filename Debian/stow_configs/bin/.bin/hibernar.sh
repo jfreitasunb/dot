2305ba057860b33b6bceb3eed49e87cc75e5ee90
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-03-25 10:37:50 -0300

    Testes.

--- Debian/stow_configs/bin/.bin/hibernar.sh
+++ Debian/stow_configs/bin/.bin/hibernar.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#! /bin/bash -x
 
 if [ "$(cat /proc/acpi/button/lid/LID/state | grep -c close)" -ge 1 ]; then
-    if ["$(cat /sys/class/power_supply/BAT0/status | grep -c DIscharging)" -ge 1]; then
-        systemctl hibernate
-    if
+    if ["$(cat /sys/class/power_supply/BAT0/status | grep -c Discharging)" -ge 1]; then
+        systemctl suspend-then-hibernate
+    fi
 fi
