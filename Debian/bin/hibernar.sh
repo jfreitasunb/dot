d83751b0a001da5b0efec54211b7b4035d11799f
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-03-26 10:37:06 -0300

    Usando o stow.

--- Debian/bin/hibernar.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-if [ "$(cat /proc/acpi/button/lid/LID/state | grep -c close)" -ge 1 ]; then
-    systemctl suspend-then-hibernate
-fi
--- Debian/bin/verifica_e_define_wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/jfreitas/.bin/
-
-if ! pgrep -x 'wallpaper.sh' >/dev/null; then
-    ./wallpaper.sh
-fi
