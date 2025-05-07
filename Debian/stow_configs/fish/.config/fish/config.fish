b5cd377137ab903117db714b5fe782f160ed82ba
José Antônio <jfreitas@unb.br>
2025-05-06 21:36:07 -0300

    Mudando para o exa.

--- Debian/stow_configs/fish/.config/fish/config.fish
+++ Debian/stow_configs/fish/.config/fish/config.fish
@@ -15,8 +15,6 @@ set -U fish_user_paths $HOME/.bin $HOME/.local/bin /var/lib/flatpak/exports/bin/
 ### EXPORT ###
 set fish_greeting # Supresses fish's intro message
 set TERM xterm-256color # Sets the terminal type
-set EDITOR "emacsclient -t -a ''" # $EDITOR use Emacs in terminal
-set VISUAL "emacsclient -c -a emacs" # $VISUAL use Emacs in GUI mode
 
 ### SET FZF DEFAULTS
 set FZF_DEFAULT_OPTS "--layout=reverse --exact --border=bold --border=rounded --margin=3% --color=dark"
@@ -154,15 +152,15 @@ alias .5='cd ../../../../..'
 # vim and emacs
 alias v='nvim'
 
-# Changing "ls" to "eza"
-alias ls='eza -al --color=always --group-directories-first' # my preferred listing
-alias la='eza -a --color=always --group-directories-first' # all files and dirs
-alias ll='eza -l --color=always --group-directories-first' # long format
-alias lt='eza -aT --color=always --group-directories-first' # tree listing
-alias l.='eza -a | egrep "^\."'
-alias l.='eza -al --color=always --group-directories-first ../' # ls on the PARENT directory
-alias l..='eza -al --color=always --group-directories-first ../../' # ls on directory 2 levels up
-alias l...='eza -al --color=always --group-directories-first ../../../' # ls on directory 3 levels up
+# Changing "ls" to "exa"
+alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first' # all files and dirs
+alias ll='exa -l --color=always --group-directories-first' # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias l.='exa -a | egrep "^\."'
+alias l.='exa -al --color=always --group-directories-first ../' # ls on the PARENT directory
+alias l..='exa -al --color=always --group-directories-first ../../' # ls on directory 2 levels up
+alias l...='exa -al --color=always --group-directories-first ../../../' # ls on directory 3 levels up
 
 # adding flags
 alias df='df -h' # human-readable sizes
