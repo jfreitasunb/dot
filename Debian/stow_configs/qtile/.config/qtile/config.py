77c032db372357d2719a346f88ee26cb3df42846
José Antônio <jfreitas@unb.br>
2025-05-07 21:06:40 -0300

    Comentários desnecessários.

--- Debian/stow_configs/qtile/.config/qtile/config.py
+++ Debian/stow_configs/qtile/.config/qtile/config.py
@@ -216,12 +216,4 @@ wl_input_rules = None
 wl_xcursor_theme = None
 wl_xcursor_size = 24
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
