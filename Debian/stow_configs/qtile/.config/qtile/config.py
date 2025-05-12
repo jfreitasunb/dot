faa7b10f515b9dc839ffb654c85ae9c88cb9e51a
José Antônio <jfreitas@unb.br>
2025-05-12 15:54:57 -0300

    Layouts.

--- Debian/stow_configs/qtile/.config/qtile/config.py
+++ Debian/stow_configs/qtile/.config/qtile/config.py
@@ -127,15 +127,15 @@ colors = colors.DoomOne
 
 layouts = [
     layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.MonadTall(),
+    layout.MonadWide(),
+    layout.Tile(),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
     # layout.RatioTile(),
-    # layout.Tile(),
     # layout.TreeTab(),
     # layout.VerticalTile(),
     # layout.Zoomy(),
