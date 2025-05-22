aba336c1969f8cc547aa2f1faa95541d66d1d7b5
José Antônio <jfreitas@unb.br>
2025-05-21 21:13:10 -0300

    Hibernação.

--- Arch/configs_stow/bin/.bin/hibernar.sh
+++ Arch/configs_stow/bin/.bin/hibernar.sh
@@ -1,7 +1,5 @@
-#! /bin/bash -x
+#! /bin/bash
 
 if [ "$(cat /proc/acpi/button/lid/LID/state | grep -c close)" -ge 1 ]; then
-    if ["$(cat /sys/class/power_supply/BAT0/status | grep -c Discharging)" -ge 1]; then
-        systemctl suspend-then-hibernate
-    fi
+    systemctl suspend-then-hibernate
 fi
