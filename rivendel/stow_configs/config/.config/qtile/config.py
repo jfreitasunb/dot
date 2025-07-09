213f7ad980850063b4046ae754408d53708262c3
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-07-09 19:02:34 -0300

    Atalho para o Nautilus.

--- rivendel/stow_configs/config/.config/qtile/config.py
+++ rivendel/stow_configs/config/.config/qtile/config.py
@@ -88,7 +88,7 @@ keys = [
     ),
     Key([mod], "k", lazy.spawn(os.path.expanduser("~/.bin/seta_keyboard_login.sh"))),
     Key([mod], "m", lazy.spawn(os.path.expanduser("~/.bin/seta_monitores_login.sh"))),
-    Key([mod, "shift"], "n", lazy.spawn("nemo")),
+    Key([mod], "n", lazy.spawn("nautilus")),
     Key([mod, "shift"], "h", lazy.spawn("systemctl hibernate")),
     Key([mod, "shift"], "r", lazy.spawn(f"{terminal} -e ranger"), desc="Open ranger"),
     Key(
