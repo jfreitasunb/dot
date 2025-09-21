4e44b80082bedba55658f2b1c4cf94c1e5eddc92
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-09-21 10:29:09 -0300

    Wallpapar no Hyprland.

--- rivendel/stow_configs/bin/.bin/wallpaper_hyprland.sh
+++ rivendel/stow_configs/bin/.bin/wallpaper_hyprland.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="$HOME/OneDrive/Pictures/Wallpapers/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+hyprctl hyprpaper unload all
+# Sets a random wallpaper with hyprpaper
+
+wallpapers=($(ls -d $HOME/OneDrive/Pictures/Wallpapers/*))
+#wallpapers+=($(ls -d /usr/share/hyprland/wall*))
+
+wall=${wallpapers[$RANDOM % ${#wallpapers[@]}]}
+
+hyprctl hyprpaper preload $wall
+hyprctl hyprpaper wallpaper ,$wall
+
+#WALLPAPER_DIR="$HOME/OneDrive/Pictures/Wallpapers/"
+#CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 # Get the name of the focused monitor with hyprctl
-FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
+#FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
 # Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+#WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
+#hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
 
-echo "preload = $wallpaper" >~/.config/hypr/hyprpaper.conf
-echo "wallpaper = , $wallpaper" >>~/.config/hypr/hyprpaper.conf
+echo "preload = $wall" >~/.config/hypr/hyprpaper.conf
+echo "wallpaper = , $wall" >>~/.config/hypr/hyprpaper.conf
