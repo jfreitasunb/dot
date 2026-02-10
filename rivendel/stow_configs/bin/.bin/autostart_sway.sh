aba643d90c12a74f81bc5ab960c8b0a6bff29d91
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-02-10 15:24:01 -0300

    Iniciando waybar com configuração para sway.

--- rivendel/stow_configs/bin/.bin/autostart_sway.sh
+++ rivendel/stow_configs/bin/.bin/autostart_sway.sh
@@ -7,3 +7,5 @@ dropbox start &
 systemctl --user start gnome-keyring-daemon.service &
 
 gnome-keyring-daemon --start --components="pkcs11,secrets,ssh" &
+
+waybar -c /home/jfreitas/.config/waybar/config_sway.jsonc &
