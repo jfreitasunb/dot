708cb38523761c4c7b40947a4c5ea23aa1d1d6ce
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-02-18 18:15:00 -0300

    Adaptando para a nova sintaxe.

--- rivendel/stow_configs/bin/.bin/wallpaper_hyprland.sh
+++ rivendel/stow_configs/bin/.bin/wallpaper_hyprland.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash -x
 
 # Define o caminho para o arquivo de bloqueio
 LOCKFILE="/tmp/wallpaper_hyprland.lock"
@@ -7,23 +7,34 @@ LOCKFILE="/tmp/wallpaper_hyprland.lock"
 # imediatamente se o bloqueio não puder ser obtido.
 # O 'exec 200>$LOCKFILE' abre o arquivo de bloqueio no descritor de arquivo 200.
 exec 200>"$LOCKFILE"
-flock -n 200 || { echo "Outra instância do script já está em execução."; exit 1; }
+flock -n 200 || {
+    echo "Outra instância do script já está em execução."
+    exit 1
+}
 
-while true; do	
-	hyprctl hyprpaper unload all
-	# Sets a random wallpaper with hyprpaper
+while true; do
 
-	wallpapers=($(ls -d $HOME/OneDrive/Pictures/Wallpapers/*))
-	#wallpapers+=($(ls -d /usr/share/hyprland/wall*))
+    wallpapers=($(ls -d $HOME/OneDrive/Pictures/Wallpapers/*))
+    #wallpapers+=($(ls -d /usr/share/hyprland/wall*))
 
-	wall=${wallpapers[$RANDOM % ${#wallpapers[@]}]}
+    wall1=${wallpapers[$RANDOM % ${#wallpapers[@]}]}
 
-	hyprctl hyprpaper preload $wall
-	hyprctl hyprpaper wallpaper ,$wall
+    wall2=${wallpapers[$RANDOM % ${#wallpapers[@]}]}
 
-	echo "preload = $wall" >~/.config/hypr/hyprpaper.conf
-	echo "wallpaper = , $wall" >>~/.config/hypr/hyprpaper.conf
-	
-	sleep 120m
+    echo "wallpaper {
+	monitor = eDP-1
+	path = $wall1
+	fit_mode = cover
+    }" >~/.config/hypr/hyprpaper.conf
+
+    echo "wallpaper {
+	monitor = HDMI-A-1
+	path = $wall2
+	fit_mode = cover
+    }" >>~/.config/hypr/hyprpaper.conf
+
+    hyprpaper
+
+    sleep 120m
 
 done
