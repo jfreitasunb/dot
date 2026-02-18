405851cf35e7685e737bbcf4611b528bc1298d98
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-02-18 18:15:57 -0300

    Corrigindo o Treesitter.

--- rivendel/stow_configs/config/.config/nvim/lua/plugins/treesitter.lua
+++ rivendel/stow_configs/config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,52 +1,29 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        },
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-            ---@diagnostic disable-next-line: missing-fields
-            configs.setup({
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                        },
-                    },
-                },
-                -- enable syntax highlighting
-                highlight = {
-                    enable = true,
-                },
-                -- enable indentation
-                indent = { enable = true },
-                -- enable autotagging (w/ nvim-ts-autotag plugin)
-                autotag = { enable = true },
-                -- ensure these language parsers are installed
-                ensure_installed = {
-                    "json",
-                    "python",
-                    "query",
-                    "php",
-                    "html",
-                    "css",
-                    "markdown",
-                    "markdown_inline",
-                    "bash",
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "dockerfile",
-                    "gitignore",
-                },
-                -- auto install above language parsers
-                auto_install = false,
-            })
-        end
-    }
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.config").setup({ -- Changed from .configs to .config
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"diff",
+				"dockerfile",
+				"gitignore",
+				"html",
+				"json",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"vim",
+				"vimdoc",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
