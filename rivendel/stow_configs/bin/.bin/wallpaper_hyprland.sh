34a275a0a2add2c3d363ed8c122023df73442a1e
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-02-19 20:13:13 -0300

    Coisas.

--- rivendel/stow_configs/bin/.bin/wallpaper_hyprland.sh
+++ rivendel/stow_configs/bin/.bin/wallpaper_hyprland.sh
@@ -13,6 +13,7 @@ flock -n 200 || {
 }
 
 while true; do
+    hyprctl hyprpaper unload all
 
     wallpapers=($(ls -d $HOME/OneDrive/Pictures/Wallpapers/*))
     #wallpapers+=($(ls -d /usr/share/hyprland/wall*))
@@ -21,19 +22,25 @@ while true; do
 
     wall2=${wallpapers[$RANDOM % ${#wallpapers[@]}]}
 
-    echo "wallpaper {
-	monitor = eDP-1
-	path = $wall1
-	fit_mode = cover
-    }" >~/.config/hypr/hyprpaper.conf
+    hyprctl hyprpaper preload $wall1
+    hyprctl hyprpaper wallpaper ,$wall1
 
-    echo "wallpaper {
-	monitor = HDMI-A-1
-	path = $wall2
-	fit_mode = cover
-    }" >>~/.config/hypr/hyprpaper.conf
+    echo "preload = $wall1" >~/.config/hypr/hyprpaper.conf
+    echo "wallpaper = , $wall1" >>~/.config/hypr/hyprpaper.conf
 
-    hyprpaper
+    #    echo "wallpaper {
+    # monitor = eDP-1
+    # path = $wall1
+    # fit_mode = cover
+    #    }" >~/.config/hypr/hyprpaper.conf
+    #
+    #    echo "wallpaper {
+    # monitor = HDMI-A-1
+    # path = $wall2
+    # fit_mode = cover
+    #    }" >>~/.config/hypr/hyprpaper.conf
+    #
+    #    hyprpaper
 
     sleep 120m
 
