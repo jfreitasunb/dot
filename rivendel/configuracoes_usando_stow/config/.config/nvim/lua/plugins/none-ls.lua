001cf307b253a43694c484c90370fdf1ce9fd372
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-04-15 18:35:27 -0300

    Teste de nova configuração do Neovim.

--- rivendel/configuracoes_usando_stow/config/.config/nvim/lua/plugins/none-ls.lua
+++ rivendel/configuracoes_usando_stow/config/.config/nvim/lua/plugins/none-ls.lua
@@ -1,56 +1,24 @@
--- Format on save and linters
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize None-ls sources
+
+---@type LazySpec
 return {
-  'nvimtools/none-ls.nvim',
-  dependencies = {
-    'nvimtools/none-ls-extras.nvim',
-    'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
-  },
-  config = function()
-    local null_ls = require 'null-ls'
-    local formatting = null_ls.builtins.formatting   -- to setup formatters
-    local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+  "nvimtools/none-ls.nvim",
+  opts = function(_, opts)
+    -- opts variable is the default configuration table for the setup function call
+    -- local null_ls = require "null-ls"
 
-    -- list of formatters & linters for mason to install
-    require('mason-null-ls').setup {
-      ensure_installed = {
-       -- 'ruff',
-        'checkmake',
-        'prettier', -- ts/js formatter
-        'eslint_d', -- ts/js linter
-        'shfmt',
-        -- 'stylua', -- lua formatter; Already installed via Mason
-        -- 'ruff', -- Python linter and formatter; Already installed via Mason
-      },
-      -- auto-install configured formatters & linters (with null-ls)
-      automatic_installation = true,
-    }
+    -- Check supported formatters and linters
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 
-    local sources = {
-      diagnostics.checkmake,
-      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
-      formatting.stylua,
-      formatting.shfmt.with { args = { '-i', '4' } },
-      require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-      require 'none-ls.formatting.ruff_format',
-    }
-
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-    null_ls.setup {
-      -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
-      sources = sources,
-      -- you can reuse a shared lspconfig on_attach callback here
-      on_attach = function(client, bufnr)
-        if client.supports_method 'textDocument/formatting' then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
-        end
-      end,
-    }
+    -- Only insert new sources, do not replace the existing ones
+    -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
+    opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+      -- Set a formatter
+      -- null_ls.builtins.formatting.stylua,
+      -- null_ls.builtins.formatting.prettier,
+    })
   end,
 }
--- rivendel/configuracoes_usando_stow/config/.config/nvim/lua/plugins/treesitter.lua
+++ rivendel/configuracoes_usando_stow/config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,15 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.config").setup({ -- Changed from .configs to .config
-			ensure_installed = {
-				"bash",
-				"c",
-				"css",
-				"diff",
-				"dockerfile",
-				"gitignore",
-				"html",
-				"json",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"vim",
-				"vimdoc",
-			},
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
 }
