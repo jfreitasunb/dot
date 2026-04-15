feea3249b5b994e9c8d7b120ba9e369c9222e900
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-04-15 18:28:21 -0300

    Pacotes que faltavam.

--- rivendel/configuracoes_usando_stow/bin/.bin/instala_fedora.sh
+++ rivendel/configuracoes_usando_stow/bin/.bin/instala_fedora.sh
@@ -16,7 +16,7 @@ sudo dnf system-upgrade reboot
 
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
-sudo dnf install nvim zsh texlive-scheme-medium cargo meld onedrive brave-browser stow bat fzf keepassxc vlc gparted alacritty -y
+sudo dnf install nvim zsh texlive-scheme-full texstudio cargo meld onedrive brave-browser stow bat fzf keepassxc vlc gparted alacritty stow gnome-tweaks gnome-extensions-app -y
 
 sudo dnf copr enable wezfurlong/wezterm-nightly
 
@@ -75,11 +75,13 @@ cd ~
 rm -rf wezterm
 
 #Instalando o yazi
+
 sudo dnf copr enable lihaohong/yazi
 
 sudo dnf install yazi -y
 
 #Virtualização
+
 sudo dnf install @virtualization -y
 
 sudo systemctl enable libvirtd
@@ -92,26 +94,29 @@ sudo dnf install mscore-fonts-all -y
 
 #Instalando o R
 
-sudo dnf install R
+sudo dnf install R -y
 
 #Instalando zoxide
 
 sudo dnf install zoxide -y
 
 #Instalando o zplug
+
 cd ~
+
 git clone https://github.com/zplug/zplug ~/.local/share/zplug
 
 #Instalando o Zathura(leitor de PDF)
+
 sudo dnf install zathura zathura-cb zathura-djvu zathura-pdf-poppler zathura-ps -y
 
 #Instalando Sublime text
+
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 
 sudo dnf config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
 sudo dnf install sublime-text -y
-
 
 #Instalando docker
 
@@ -124,22 +129,20 @@ sudo systemctl enable docker
 sudo usermod -aG docker jfreitas
 
 #Instalando Flatpaks
+
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install flathub com.bitwarden.desktop -y
 
-flatpak install flathub org.gimp.GIMP -y
-
 flatpak install flathub io.github.shiftey.Desktop -y
-
-flatpak install flathub com.github.PintaProject.Pinta -y
 
 flatpak install flathub com.spotify.Client -y
 
-flatpak install flathub com.github.xournalpp.xournalpp -y
-
 flatpak install flathub org.geogebra.GeoGebra -y
 
+#Instalando GIMP, Pinta e Xournalpp
+
+sudo dnf install gimp pinta xournalpp -y
 
 #Arquivos de configuração
 
@@ -147,7 +150,7 @@ cd ~
 
 rm -rf ~/.config
 
-cd ~/GitHub/dot/rivendel/stow_configs/
+cd ~/GitHub/dot/rivendel/configuracoes_usando_stow/
 
 stow -t /home/jfreitas/ *
 
@@ -158,10 +161,16 @@ fc-cache -f -v
 cd ~
 
 #Instalando atuin
+
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
 #Instalando starship
+
 curl -sS https://starship.rs/install.sh | sh
+
+#Instalando uv
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
 #TMUX
 
@@ -175,11 +184,16 @@ sudo chsh -s $(which zsh) jfreitas
 sudo dnf install sway wofi waybar swaylock wlogout network-manager-applet dunst -y
 
 #Ativando configurações personalizadas
+
 sudo rm /etc/default/grub
 
-sudo ln -s ~/GitHub/dot/rivendel/grub/grub /etc/default/grub
+sudo ln -s ~/GitHub/dot/rivendel/grub/grub-FEDORA /etc/default/grub
 
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+sudo ln -s ~/GitHub/dot/rivendel/dracut.conf.d/resume.conf /etc/dracut.conf.d
+
+sudo dracut -f
 
 sudo rm -rf /etc/systemd/sleep.conf
 
