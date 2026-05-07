d09023f6e68e260767d08e2c1156593c7308b379
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-05-07 11:31:46 -0300

    Backup das configurações do Gnome no GIT.

--- rivendel/configuracoes_usando_stow/bin/.bin/lista_pacotes_debian.sh
+++ rivendel/configuracoes_usando_stow/bin/.bin/lista_pacotes_debian.sh
@@ -2,16 +2,22 @@
 
 NOME_BACKUP_GNOME="debian_gnome_settings_"$(date +%Y-%m-%d)".ini"
 
+NOME_BACKUP_GNOME_GIT="debian_gnome_settings.ini"
+
 NOME_BACKUP="debian_lista_pacotes_instalados_APT_"$(date +%Y-%m-%d)".lst"
 
 LOCAL_BACKUP="/home/jfreitas/OneDrive/Backups/Debian/Backup-Diario/"
 
-dpkg-query -f '${binary:Package}\n' -W > "$LOCAL_BACKUP""$NOME_BACKUP"
+LOCAL_BACKUP_GIT="/home/jfreitas/GitHub/dot/rivendel/gnome/"
+
+dpkg-query -f '${binary:Package}\n' -W >"$LOCAL_BACKUP""$NOME_BACKUP"
 
 find "$LOCAL_BACKUP" -type f -mtime +10 -delete
 
 #salva configuraçoes do gnome
-dconf dump / > "$LOCAL_BACKUP""$NOME_BACKUP_GNOME"
+dconf dump / >"$LOCAL_BACKUP""$NOME_BACKUP_GNOME"
+
+dconf dump / >"$LOCAL_BACKUP_GIT""$NOME_BACKUP_GNOME_GIT"
 
 #importa as configuraçoes do gnome
 #dconf load / < dconf-settings.ini
