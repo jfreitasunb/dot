3884e2f9b0442fc37623802c51d5d0d8dad66836
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-06-22 20:49:42 -0300

    Instalando o docker.

--- numenor/nixos/configuration.nix
+++ numenor/nixos/configuration.nix
@@ -105,15 +105,16 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  #Qemu e virtmanager
+  #Qemu, virtmanager e docker
   virtualisation.libvirtd.enable = true;
-
+  programs.virt-manager.enable = true;
+  virtualisation.docker.enable = true;
   
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."jfreitas" = {
     isNormalUser = true;
     description = "José Antônio";
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker"];
     packages = with pkgs; [
       kdePackages.kate
     #  thunderbird
