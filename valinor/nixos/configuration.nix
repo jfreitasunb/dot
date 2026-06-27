6e1b4539bf27b79192d4c99023170c814c1f0e84
Jose Antonio <jfreitas@unb.br>
2026-06-27 15:03:38 -0300

    Configuração de som.

--- valinor/nixos/configuration.nix
+++ valinor/nixos/configuration.nix
@@ -77,16 +77,24 @@
   # Enable CUPS to print documents.
   services.printing.enable = false;
 
-  # Enable sound.
+  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
-  # OR
+  security.rtkit.enable = true;
   services.pipewire = {
-     enable = true;
-     pulse.enable = true;
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jfreitas= {
