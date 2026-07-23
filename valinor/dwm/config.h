1a937dfa75c9693f90fc72846cd94e13f57914a7
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2026-07-23 19:16:37 -0300

    Mudando o terminal para o ST.

--- valinor/dwm/config.h
+++ valinor/dwm/config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
-	"st", NULL,
+	"setxkbmap", "-layout", "br", "-variant", "thinkpad", NULL,
 	NULL /* terminate */
 };
 
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *rofi[] = { "rofi", "-show", "drun", "-theme ~/.config/rofi/config.rasi", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]  = { "brave-browser", NULL };
 
 static const Key keys[] = {
